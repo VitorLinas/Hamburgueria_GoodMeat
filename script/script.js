955e9f8a75e284b0726dfbc8b14548120a2b2001
Vitor Gabriel Linas <103148031+VitorLinas@users.noreply.github.com>
2022-09-10 15:03:44 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function cadastrar(){
+    window.alert("Formulário ilustrativo!");
+}
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('#cardapio img', {
+    duration: 2000,
+    rotate:{x:0, y:80, z: 0}
+});
