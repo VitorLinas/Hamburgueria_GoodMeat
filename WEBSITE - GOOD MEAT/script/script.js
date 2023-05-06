f9c146ecd1fc5d8ee84b03767ebdf73e5020eaa0
Vitor Gabriel Linas <103148031+VitorLinas@users.noreply.github.com>
2023-05-06 19:32:03 -0300

    Add files via upload

---
+++ WEBSITE - GOOD MEAT/script/script.js
@@ -0,0 +1,16 @@
+function cadastrar(){
+    window.alert("Formulário ilustrativo!");
+}
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('#cardapio img', {
+    duration: 2000,
+    rotate:{x:0, y:80, z: 0},
+    once: true,
+    delay: 300,
+    reset: false
+});
+
+
+
+
